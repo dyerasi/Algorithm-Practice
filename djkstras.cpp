334699864362485a93e78ca993b72e0df742f4b9
Devin Yerasi <devin@alichbot.com>
2019-11-03 22:15:00 -0800

    psuedocode added

--- djkstras.cpp
+++ djkstras.cpp
@@ -1,6 +1,19 @@
 #include <stdio.h>
 using namespace std;
 
+//Pseudocode
+// -let s be root node, set d(s)=0
+// -set distance of all other vertices to -1(infinity)
+// -let visitingNode = s
+// -While their is an unvisited node in g
+//     -for each edge coming out of visitingNode
+//         -if the visitedNode neighbor has not been visited and if d(visitingNode) + d(edge) < d(neighbor)
+//             -set d(neigbor)=d(visitingNode) + d(edge)
+//     visited[visitingNode] = true
+//     remove visitingNode from G
+//     visitingNode = unvisited node with least weight
+
+
 
 //testing
 int main(){
